c7d8d6758e9c149637446360b5ab57a71127d58a
Shohei Miyahara <m.shohei1216@gmail.com>
2021-03-31 08:50:44 +0900

    トップページ進行

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Components/Navbar'
-import Header from './Components/Header';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <>
-    < Navbar />
-    <Header />
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              }
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                with: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
     </>
   );
 }
